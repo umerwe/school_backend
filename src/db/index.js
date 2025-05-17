b11be544d67d5a577f6614ecd27c3892a9fd684c
umerwe <uemyy1@gmail.com>
2025-05-17 16:38:43 +0500

    updated

--- src/db/index.js
+++ src/db/index.js
@@ -3,7 +3,7 @@ import mongoose from 'mongoose';
 export const connectDB = async () => {
     try {
         await mongoose.connect(`${process.env.MONGODB_URL}/${process.env.DB_NAME}`);
-        console.log('✅ MongoDB Connected Successfully');
+        console.log('✅ MongoDB Connected Successfully.');
     } catch (error) {
         console.error(`❌ Database Connection Error: ${error.message}`);
         process.exit(1); // Exit the app if DB connection fails
