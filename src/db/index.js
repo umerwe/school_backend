9cf2e289743067bb92c2786b807563d47c612eff
umerwe <uemyy1@gmail.com>
2025-05-17 12:52:20 +0500

    updated

--- src/db/index.js
+++ src/db/index.js
@@ -2,7 +2,7 @@ import mongoose from 'mongoose';
 
 export const connectDB = async () => {
     try {
-        await mongoose.connect(`${process.env.MONGODB_URL}/${process.env.DB_NAME}`);
+        await mongoose.connect('mongodb://127.0.0.1:27017/school');
         console.log('✅ MongoDB Connected Successfully');
     } catch (error) {
         console.error(`❌ Database Connection Error: ${error.message}`);
